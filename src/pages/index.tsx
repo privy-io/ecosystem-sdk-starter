cc12ebae94f18061dfff92968b6e904aa5baea16
Jainil Sutaria <jainil@privy.io>
2024-11-21 11:07:06 -0500

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,50 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+import { AppToaster } from "@/components/Toast";
+import { StrawberryWalletProvider } from "@/strawberry-sdk";
+import { InteractiveHome } from "@/components/InteractiveHome";
+import { InfoCards } from "@/components/InfoCards";
+
+const abcFavorit = localFont({
+  src: "./fonts/ABCFavoritExtendedVariable-Trial.ttf",
+  variable: "--font-abc-favorit",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <StrawberryWalletProvider>
+      <div
+        className={`${abcFavorit.variable} ${inter.className} items-center justify-items-center min-h-screen p-10 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      >
+        <AppToaster />
+        <main className="flex flex-col md:flex-row gap-8 row-start-2 items-center sm:items-start justify-around w-full">
+          <div className="flex flex-col w-[415px] gap-6 md:w-full">
+            <div className="flex flex-col w-[220px]">
+              <Image
+                src="/strawberry.png"
+                alt="Strawberry Logo"
+                width={100}
+                height={100}
+              />
+              <h1 className="font-favorit font-bold text-5xl">
+                Strawberry Fields
+              </h1>
+            </div>
+            <h4 className="text-balance font-medium text-md mt-3 max-w-[600px] mb-3">
+              Privy makes it easy to provision your own ecosystem wallet. Create
+              an ecosystem login for every developer on your app/chain, and make
+              it easily reusable across the web.
+            </h4>
+            <InteractiveHome />
+          </div>
+          <InfoCards />
+        </main>
+      </div>
+    </StrawberryWalletProvider>
   );
 }
